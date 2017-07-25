19e750d2f37d88c44c05483332dd70bb61e86488
Michal_Partacz <michal_partacz@epam.com>
2017-07-25 09:18:26 +0200

    Created a fxml file which (will) contain UI elements, a Client class which loads this file and a fxml's corresponding controller class;

---
+++ src/main/java/com/spanish_inquisition/battleship/client/MainMenuController.java
@@ -0,0 +1,9 @@
+package com.spanish_inquisition.battleship.client;
+
+/**
+ * @author Michal_Partacz
+ * A controller for a fxml file placed in resources folder. The methods annotated with @FXML react to events specified
+ * in the file's UI elements.
+ */
+public class MainMenuController {
+}
