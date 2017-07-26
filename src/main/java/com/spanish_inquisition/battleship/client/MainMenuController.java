a6a071e024087f0a942885e545a7af9bdccaf206
Michal_Partacz <michal_partacz@epam.com>
2017-07-26 09:39:10 +0200

    Added a method to the socketClient which will send to the server a string; Added a text Field which will allow the user to enter the text and send it to the server;

--- src/main/java/com/spanish_inquisition/battleship/client/MainMenuController.java
+++ src/main/java/com/spanish_inquisition/battleship/client/MainMenuController.java
@@ -1,7 +1,16 @@
 package com.spanish_inquisition.battleship.client;
 
+import com.spanish_inquisition.battleship.client.network.SocketClient;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.util.logging.Level;
+
+import static com.spanish_inquisition.battleship.common.AppLogger.logger;
 
 /**
  * @author Michal_Partacz
@@ -9,16 +18,46 @@ import javafx.scene.control.Button;
  * in the file's UI elements.
  */
 public class MainMenuController {
-
     public Button featureButton;
+    public Label gameStatusLabel;
+    public TextField nameTextField;
+    public VBox centralVBox;
+
+    public SocketClient socketClient;
+
     /**
      * This method is run automatically right after the fxml file's loaded
      */
     @FXML
-    public void initialize() {}
+    public void initialize() {
+        this.socketClient = new SocketClient();
+        try {
+            this.socketClient.setUpStreamsAndOpenSocket();
+        } catch (IOException e) {
+            logger.log(Level.WARNING, "The client could not connect to the server: " + e.getMessage());
+            gameStatusLabel.setText("I could not connect to the server :(");
+        }
+        initializeElementsAfterServerConnection();
+    }
 
+    private void initializeElementsAfterServerConnection() {
+        this.centralVBox.setVisible(true);
+    }
+
+    /**
+     * After the button is clicked the information entered in the textField is sent to the server
+     */
     @FXML
     public void onFeatureButtonClicked() {
-        System.out.println("Clicked!");
+        this.sendTextToSocket(this.nameTextField.getText());
+    }
+
+    private void sendTextToSocket(String text) {
+        this.socketClient.sendStringToServer(text);
+    }
+
+    @FXML
+    public void onNameTextFieldEntered() {
+        this.sendTextToSocket(this.nameTextField.getText());
     }
 }
--- src/main/java/com/spanish_inquisition/battleship/client/network/SocketClient.java
+++ src/main/java/com/spanish_inquisition/battleship/client/network/SocketClient.java
@@ -8,23 +8,33 @@ import java.nio.charset.StandardCharsets;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import static com.spanish_inquisition.battleship.common.AppLogger.DEFAULT_LEVEL;
+
 /**
  * @author Michal_Partacz
  */
 public class SocketClient {
     private final static String HOST_NAME = "localhost";
-    private final static int PORT = 6666;
+    final static int PORT = 6666;
     private final static Logger logger = Logger.getLogger(AppLogger.class.getName());
 
-    private Level level = Level.CONFIG;
     private Socket socket;
     private BufferedReader input;
     private PrintWriter output;
 
-    void setUpStreamsAndOpenSocket() throws IOException {
-        logger.log(level, "Setting up streams and socket");
+    public void setUpStreamsAndOpenSocket() throws IOException {
+        logger.log(DEFAULT_LEVEL, "Setting up streams and socket");
         socket = new Socket(HOST_NAME, PORT);
         input = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
         output = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8), true);
+    }
+
+    public void sendStringToServer(String string) {
+        try {
+            this.output.println(string);
+            logger.log(DEFAULT_LEVEL, "sent string to server = " + string);
+        } catch (NullPointerException e) {
+            logger.log(Level.WARNING, "Could not send " + string + " to server");
+        }
     }
 }
