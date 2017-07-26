8c2d0860fa1387b4c7b983753a6f94b48813e192
Michal_Partacz <michal_partacz@epam.com>
2017-07-26 12:04:48 +0200

    Added test for sending string to server

--- src/test/java/com/spanish_inquisition/battleship/client/network/SocketClientTest.java
+++ src/test/java/com/spanish_inquisition/battleship/client/network/SocketClientTest.java
@@ -6,17 +6,37 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import static org.testng.Assert.assertNotNull;
-import static org.testng.Assert.assertNull;
+import static org.testng.Assert.*;
 
 /**
  * @author Michal_Partacz
  */
 @Test
 public class SocketClientTest {
+    @Test
+    public void testSendStringToServer() throws Exception {
+        byte[] emptyPayload = new byte[1001];
+
+        final Socket socket = mock(Socket.class);
+        final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(emptyPayload);
+        when(socket.getOutputStream()).thenReturn(byteArrayOutputStream);
+        when(socket.getInputStream()).thenReturn(byteArrayInputStream);
+
+        SocketClient socketClient = new SocketClient();
+        socketClient.socket = socket;
+        socketClient.setUpStreamsOnSocket();
+        assertEquals(byteArrayOutputStream.size(), 0);
+        String stringToServer = "Hello server!";
+        String stringExpectedFromServer = stringToServer + "\r\n";
+        socketClient.sendStringToServer(stringToServer);
+        assertNotEquals(byteArrayOutputStream.size(), 0);
+        assertEquals(byteArrayOutputStream.toByteArray(), stringExpectedFromServer.getBytes(StandardCharsets.UTF_8));
+    }
 
     @Test(expectedExceptions = IOException.class)
     public void testNewSocketClient_withoutServer() throws Exception {
