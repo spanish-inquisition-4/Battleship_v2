4221a81a04e831cc5acc80e51a8774bb3506a74e
Michal <azet89@gmail.com>
2017-07-27 16:27:00 +0200

    Update RunServerAndTwoClients.sh

--- RunServerAndTwoClients.sh
+++ RunServerAndTwoClients.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-mvn exec:java -Dexec.mainClass="com.spanish_inquisition.battleship.server.BattleshipServer" &
+mvn exec:java -Dexec.mainClass="com.spanish_inquisition.battleship.server.ServerMain" &
 mvn exec:java -Dexec.mainClass="com.spanish_inquisition.battleship.client.Client" &
 mvn exec:java -Dexec.mainClass="com.spanish_inquisition.battleship.client.Client" &
