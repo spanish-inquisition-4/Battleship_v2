b8cc5d0ac5acd461e804b62870f3a6a37128f9dc
Piotr Sekula <piotr92345@o2.pl>
2017-07-28 08:28:23 +0200

    Write test for calculating index from coordinates in AdjacentFieldCalc

--- src/test/java/com/spanish_inquisition/battleship/common/AdjacentFieldsCalcTest.java
+++ src/test/java/com/spanish_inquisition/battleship/common/AdjacentFieldsCalcTest.java
@@ -20,6 +20,24 @@ public class AdjacentFieldsCalcTest {
         };
     }
 
+    @DataProvider(name = "CoordinatesProvider")
+    public Object[][] coordinatesProvider(){
+        return new Object[][] {
+                {new int[] {-5, 3}, -1 },
+                {new int[] {15, 3}, -1 },
+                {new int[] {5, -3}, -1 },
+                {new int[] {5, 13}, -1 },
+                {new int[] {0, 10}, -1 },
+                {new int[] {10, 0}, -1 },
+                {new int[] {0, 0}, 0 },
+                {new int[] {0, 9}, 90 },
+                {new int[] {1, 1}, 11 },
+                {new int[] {3, 6}, 63 },
+                {new int[] {7, 5}, 57 },
+                {new int[] {9, 9}, 99 },
+        };
+    }
+
     @Test(dataProvider = "IndexesProvider")
     public void shouldTranslateIndexToCoordinates(int index, int[] expectedCoordinates){
         // When
@@ -28,5 +46,14 @@ public class AdjacentFieldsCalcTest {
         // Then
         Assert.assertEquals(expectedCoordinates[0], calculatedCoordinates[0]);
         Assert.assertEquals(expectedCoordinates[1], calculatedCoordinates[1]);
+    }
+
+    @Test(dataProvider = "CoordinatesProvider")
+    public void shouldTranslateCoordinatesToIndex(int[] coordinates, int expectedIndex){
+        // When
+        int calculatedIndex = AdjacentFieldsCalc.translateCoordinatesToIndex(coordinates[0],coordinates[1]);
+
+        // Then
+        Assert.assertEquals(calculatedIndex, expectedIndex);
     }
 }
