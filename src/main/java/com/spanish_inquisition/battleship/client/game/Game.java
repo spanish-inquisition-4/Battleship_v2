21fb6271bdece022a4454351827330a00d57ba5a
Michal_Partacz <michal_partacz@epam.com>
2017-08-02 15:22:23 +0200

    Refactored this ugly nested if-else but not very nicely; In future, there will be need for extensive refactoring based on polymorphism as seen in server right now;

--- src/main/java/com/spanish_inquisition/battleship/client/game/Game.java
+++ src/main/java/com/spanish_inquisition/battleship/client/game/Game.java
@@ -3,11 +3,11 @@ package com.spanish_inquisition.battleship.client.game;
 import com.spanish_inquisition.battleship.client.board.BoardController;
 import com.spanish_inquisition.battleship.client.network.ResponsesBus;
 import com.spanish_inquisition.battleship.client.network.SocketClient;
-import com.spanish_inquisition.battleship.common.Header;
 import com.spanish_inquisition.battleship.common.NetworkMessage;
 
 import static com.spanish_inquisition.battleship.common.AppLogger.DEFAULT_LEVEL;
 import static com.spanish_inquisition.battleship.common.AppLogger.logger;
+import static com.spanish_inquisition.battleship.common.Header.*;
 
 /**
  * @author Michal_Partacz
@@ -39,29 +39,28 @@ public class Game {
         return boardController.getMessageForServer();
     }
 
-    public void runTheGame() throws InterruptedException {
-        if (this.socketClient != null) {
-            while (true) {
-                    Thread.sleep(GAME_LOOP_SLEEP);
-                if (this.responsesBus.hasServerResponses()) {
-                    NetworkMessage message = this.responsesBus.getAServerResponse();
-                    System.out.println("s = " + message);
-                    if (message == null) {
-                        continue;
+    public void runTheGame() throws InterruptedException, NullPointerException {
+        game_loop:
+        while (true) {
+            Thread.sleep(GAME_LOOP_SLEEP);
+            if (this.responsesBus.hasServerResponses()) {
+                NetworkMessage message = this.responsesBus.getAServerResponse();
+                if (message == null) {
+                    continue;
+                }
+                switch (message.getHeader()) {
+                    case PLAYER_TURN: { /*notify about his move */ }
+                    case OPPONENT_TURN: { /* notify that it is not his turn */ }
+                    case GAME_WON: {
+                        break game_loop;/*notify the player he won or lost */
                     }
-                    if (message.getHeader() == Header.PLAYER_TURN) {
-                        //notify about his move
-                    } else if (message.getHeader() == Header.OPPONENT_TURN) {
-                        // notify that it is not his turn
-                    } else if (Header.isResponseFieldChanging(message.getHeader())) {
-                        // make changes to the opponent's board
-                    } else if (message.getHeader() == Header.GAME_WON) {
-                        break;
-                        // notify the player he won or lost
-                    }
+                }
+                if (isResponseFieldChanging(message.getHeader())) {
+                    // make changes to the opponent's board
                 }
             }
         }
+
     }
 
     public void acceptPlayersName(String name) {
